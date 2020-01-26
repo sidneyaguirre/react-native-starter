35b83bed3410947e1db78e2b01e5ac389a30d661
sidneyaguirre <sidney.aguirrec@gmail.com>
2020-01-26 18:17:25 -0500

    navigation between views

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,18 +2,24 @@ import React from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+
+  console.log(props);
   
   return (
     <View>
       <Text style={styles.text}>Sidney Aguirre</Text>
       <Button 
-        onPress={() => console.log('Components pressed!')} 
+        onPress={() => props.navigation.navigate('Components')} //to go to the "Components" component. 
         title="Go to Components" 
       />
-      <TouchableOpacity onPress={() => console.log('List pressed!')}>
+      <Button 
+        onPress={() => props.navigation.navigate('List')} //to go to the "List" component. 
+        title="Go to List" 
+      />
+      {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
         <Text>Go to List</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 }; 
