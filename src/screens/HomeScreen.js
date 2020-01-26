74a148d2eda11fbc525233f5d8eab5d06f366f7e
sidneyaguirre <sidney.aguirrec@gmail.com>
2020-01-26 18:20:40 -0500

    destructuring the props object received in HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,19 +2,17 @@ import React from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = (props) => {
-
-  console.log(props);
+const HomeScreen = ( { navigation }) => {
   
   return (
     <View>
       <Text style={styles.text}>Sidney Aguirre</Text>
       <Button 
-        onPress={() => props.navigation.navigate('Components')} //to go to the "Components" component. 
+        onPress={() => navigation.navigate('Components')} //to go to the "Components" component. 
         title="Go to Components" 
       />
       <Button 
-        onPress={() => props.navigation.navigate('List')} //to go to the "List" component. 
+        onPress={() => navigation.navigate('List')} //to go to the "List" component. 
         title="Go to List" 
       />
       {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
